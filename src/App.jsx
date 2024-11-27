880ca41f8b36b412022beb6474e24ebe20681c1c
Artem Erusov <erusovav@gmail.com>
2024-11-27 14:44:43 +0300

    made header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import './assets/css/main.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <>
-     <div className='test'>
-      <p>Привет Мир!!!</p>
-     </div>
+     <Header />
     </>
   )
 }
