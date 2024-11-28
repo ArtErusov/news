75d693447e995dbcb4cf4a458641201f1cc7efad
Artem Erusov <erusovav@gmail.com>
2024-11-28 17:40:50 +0300

    Transferred data to the card

--- src/components/NewsBanner/NewsBanner.jsx
+++ src/components/NewsBanner/NewsBanner.jsx
@@ -5,9 +5,9 @@ import styles from './styles.module.css';
 function NewsBanner({item}) {
   return (
   <div>
-    {/* <Image image={item.image}/> */}
-    {/* <h3>{item.title}</h3>
-    <p>{formatTimeAgo(item.published)} by {item.author}</p> */}
+    <Image image={item.image}/>
+    <h3>{item.title}</h3>
+    <p>{formatTimeAgo(item.published)} by {item.author}</p>
   </div>
   )
 }
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,15 +1,16 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import NewsBanner from '../../components/NewsBanner/NewsBanner';
 import styles from './styles.module.css';
 import { getNews } from '../../api/apiNews';
 
 function Main() {
+  const [news, setNews] = useState([]);
 
   useEffect(() => {
     const fetchNews = async () => {
         try {
-            const news = await getNews();
-            console.log(news);
+            const response = await getNews();
+           setNews(response.news);
         } catch (error) {
             console.log('Не удалось получить новости (Main):', error);
         }
@@ -20,7 +21,7 @@ function Main() {
 
   return (
  <>
-<NewsBanner />
+{news.length > 0 ? <NewsBanner  item={news[3]}/> : null}
  </>
   )
 }
