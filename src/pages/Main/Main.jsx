2ed1d6c4488880397b241c14c962d3d4dde8dcaf
Artem Erusov <erusovav@gmail.com>
2024-11-28 17:22:35 +0300

    Made data retrieval from API

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,7 +1,23 @@
+import { useEffect } from 'react';
 import NewsBanner from '../../components/NewsBanner/NewsBanner';
 import styles from './styles.module.css';
+import { getNews } from '../../api/apiNews';
 
 function Main() {
+
+  useEffect(() => {
+    const fetchNews = async () => {
+        try {
+            const news = await getNews();
+            console.log(news);
+        } catch (error) {
+            console.log('Не удалось получить новости (Main):', error);
+        }
+    };
+
+    fetchNews();
+}, []); 
+
   return (
  <>
 <NewsBanner />
