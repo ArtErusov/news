0fc0d9d5bc6201edf701d90a8c42b0b867dffa89
Artem Erusov <erusovav@gmail.com>
2024-12-02 17:21:44 +0300

    made the element NewsList

---
+++ src/components/NewsList/components/NewsItem/NewsItem.jsx
@@ -0,0 +1,31 @@
+import styles from "./styles.module.css";
+import { formatTimeAgo } from "../../../../helpers/formatTimeAgo";
+
+function NewsItem({ item }) {
+  // Моя функция проверяет длину автора чтоб не было переноса на 2 строки
+  // можно вынести в отдельную функцию
+  const LengthCheck = (text) =>{
+    if (text.length > 17) {
+      return text.slice(0, 14) + ' ...';
+  }
+    return text;
+  }
+// ====================
+console.log(LengthCheck(item.author))
+  return (
+    <li className={styles.item}>
+      <div
+        className={styles.wrapper}
+        style={{ backgroundImage: `url(${item.image})` }}
+      ></div>
+      <div className={styles.info}>
+        <h3 className={styles.title}>{item.title}</h3>
+        <p className={styles.extra}>
+          {formatTimeAgo(item.published)} by {LengthCheck(item.author)}
+        </p>
+      </div>
+    </li>
+  );
+}
+
+export default NewsItem;
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,34 +1,41 @@
-import { useEffect, useState } from 'react';
-import NewsBanner from '../../components/NewsBanner/NewsBanner';
-import styles from './styles.module.css';
-import { getNews } from '../../api/apiNews';
+import { useEffect, useState } from "react";
+import NewsBanner from "../../components/NewsBanner/NewsBanner";
+import styles from "./styles.module.css";
+import { getNews } from "../../api/apiNews";
+import NewsList from "../../components/NewsList/NewsList";
 
 function Main() {
   const [news, setNews] = useState([]);
 
   useEffect(() => {
     const fetchNews = async () => {
-        try {
-            const response = await getNews();
-           setNews(response.news);
-        } catch (error) {
-            console.log('Не удалось получить новости (Main):', error);
-        }
+      try {
+        const response = await getNews();
+        setNews(response.news);
+      } catch (error) {
+        console.log("Не удалось получить новости (Main):", error);
+      }
     };
 
     fetchNews();
-}, []); 
+  }, []);
 
-return (
-  <div className={styles.container}>
-    {news.length > 0 && (
-      <div className={styles.banners}>
-        <NewsBanner item={news[0]} />
-        <NewsBanner item={news[3]} />
+  return (
+    <>
+      <div className={styles.container}>
+        {news.length > 0 && (
+          <div className={styles.banners}>
+            <NewsBanner item={news[0]} />
+            <NewsBanner item={news[3]} />
+          </div>
+        )}
       </div>
-    )}
-  </div>
-)
+      <div className={styles.divider}></div>
+      <div className={styles.container}>
+        <NewsList news={news} />
+      </div>
+    </>
+  );
 }
 
-export default Main
+export default Main;
