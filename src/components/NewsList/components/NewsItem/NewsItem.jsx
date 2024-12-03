b7089536a5c04136999a5cf7b12485b81d5b2587
Artem Erusov <erusovav@gmail.com>
2024-12-03 16:21:55 +0300

    moved it to a separate function for checking the length

--- src/components/NewsList/components/NewsItem/NewsItem.jsx
+++ src/components/NewsList/components/NewsItem/NewsItem.jsx
@@ -1,28 +1,20 @@
 import styles from "./styles.module.css";
 import { formatTimeAgo } from "../../../../helpers/formatTimeAgo";
+import { LengthCheck } from "../../../../helpers/lengthCheck";
 
 function NewsItem({ item }) {
-  // Моя функция проверяет длину автора чтоб не было переноса на 2 строки
-  // можно вынести в отдельную функцию
-  const LengthCheck = (text) =>{
-    if (text.length > 17) {
-      return text.slice(0, 14) + ' ...';
-  }
-    return text;
-  }
-// ====================
+
 console.log(LengthCheck(item.author))
   return (
     <li className={styles.item}>
-      <div
-        className={styles.wrapper}
-        style={{ backgroundImage: `url(${item.image})` }}
-      ></div>
+      <div className={styles.wrapper} style={{ backgroundImage: `url(${item.image})` }}></div>
+
       <div className={styles.info}>
-        <h3 className={styles.title}>{item.title}</h3>
-        {/* Сделать в функции второй аргумент который принемает длину и укоротить некоторые элементы h3 */}
-        <p className={styles.extra}>
-          {formatTimeAgo(item.published)} by {LengthCheck(item.author)}
+        <h3 className={styles.title}>
+          {LengthCheck(item.title, 90)}
+        </h3>
+        <p className={styles.extra}> 
+          {formatTimeAgo(item.published)} by {LengthCheck(item.author, 16)}
         </p>
       </div>
     </li>
